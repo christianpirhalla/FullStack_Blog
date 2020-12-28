990fe3f3594fda2892c301b11eda6790031ced90
munir <msanzar87@gmail.com>
2020-12-28 17:02:27 -0500

    tagStuff

--- BlogProject/src/main/java/com/example/BlogProject/entities/Tag.java
+++ BlogProject/src/main/java/com/example/BlogProject/entities/Tag.java
@@ -1,7 +1,16 @@
 package com.example.BlogProject.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String tagName;
+
+    @ManyToMany(mappedBy = "blogPost")
+    private Set<BlogPost> blogs = new HashSet<>();
 }
