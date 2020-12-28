31995c98ef3caefb8a916229eaa004f70f504091
Christian Pirhalla <chrispir@udel.edu>
2020-12-28 13:16:40 -0500

    project skeleton

---
+++ BlogProject/src/main/java/com/example/BlogProject/repositories/TagRepository.java
@@ -0,0 +1,7 @@
+package com.example.BlogProject.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository {
+}
---
+++ BlogProject/src/main/java/com/example/BlogProject/services/BlogPostService.java
@@ -0,0 +1,7 @@
+package com.example.BlogProject.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogPostService {
+}
