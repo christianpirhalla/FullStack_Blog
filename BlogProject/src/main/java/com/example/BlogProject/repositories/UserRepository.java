f837408b25ca5fe1a6b386eb50f5c7d2dedad458
Earl Atta-Fynn <Earl.Atta.Fynn@gmail.com>
2020-12-28 15:17:41 -0500

    User model and User service built

--- BlogProject/src/main/java/com/example/BlogProject/repositories/UserRepository.java
+++ BlogProject/src/main/java/com/example/BlogProject/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.BlogProject.repositories;
 
+import com.example.BlogProject.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
