01a986eaf18f1aebd4873fe8741417bcad989a61
DavidToribio1 <dtmuzik1@gmail.com>
2020-12-28 16:22:37 -0500

    base

--- BlogProject/src/main/java/com/example/BlogProject/repositories/UserRepository.java
+++ BlogProject/src/main/java/com/example/BlogProject/repositories/UserRepository.java
@@ -1,8 +1,15 @@
 package com.example.BlogProject.repositories;
 
+import com.example.BlogProject.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
-    //made branch
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmailAddress(String emailAddress);
 }
--- BlogProject/src/main/java/com/example/BlogProject/services/BlogPostService.java
+++ BlogProject/src/main/java/com/example/BlogProject/services/BlogPostService.java
@@ -1,7 +1,55 @@
 package com.example.BlogProject.services;
 
+import com.example.BlogProject.entities.BlogPost;
+import com.example.BlogProject.entities.Tag;
+import com.example.BlogProject.repositories.BlogPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class BlogPostService {
+    @Autowired
+    private BlogPostRepository blogPostRepository;
+
+    @Autowired
+    BlogPost blogPost;
+
+    public List<BlogPost> findAll(){
+        return blogPostRepository.findAll();
+    }
+
+
+    public BlogPost FindAllByTag(String tagName){
+
+        return null;
+    }
+
+    public void deleteByID(Long id){
+        blogPostRepository.delete(id);
+    }
+
+
+    public BlogPost createBlogPost(BlogPost blogPost){
+        return blogPostRepository.save(blogPost);
+    }
+
+    public BlogPost updateBlogPost(BlogPost post){
+        return blogPostRepository.save(post);
+
+
+    }
+    public BlogPost findAllById(Long userID){
+        return null;
+    }
+    public BlogPost findAllByUsername(String username){
+        return null;
+    }
+
+
+
+
+
 }
