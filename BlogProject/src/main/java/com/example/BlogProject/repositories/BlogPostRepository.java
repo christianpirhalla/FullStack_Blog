d203f59dc49e13368618226642d657355c546a85
Christian Pirhalla <chrispir@udel.edu>
2020-12-28 20:38:37 -0500

    BlogPost & repo

--- BlogProject/src/main/java/com/example/BlogProject/repositories/BlogPostRepository.java
+++ BlogProject/src/main/java/com/example/BlogProject/repositories/BlogPostRepository.java
@@ -1,7 +1,14 @@
 package com.example.BlogProject.repositories;
 
+import com.example.BlogProject.entities.BlogPost;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BlogPostRepository {
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+    public List<BlogPost> findByUser_Id(Long userId);
+    //How is this going to work?
+    public List<BlogPost> findByTags_Name(String tagName);
 }
